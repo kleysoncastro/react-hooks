9a7ce56cfdde744a29b1fd6d89b0c2f9eefe8767
kleyson castro <kleysoncastro99@gmail.com>
2019-11-08 11:23:00 -0300

    chore: useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return <p>Hello word</p>;
+  const [tecnoloigas, SetTecnoloigas] = useState([]);
+
+  const [newTch, setNewTch] = useState('');
+
+  function hadleAdd() {
+    SetTecnoloigas([...tecnoloigas, newTch]);
+    setNewTch('');
+  }
+
+  useEffect(() => {
+    const getTechs = localStorage.getItem('tech');
+
+    if (getTechs) {
+      SetTecnoloigas(JSON.parse(getTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tecnoloigas));
+  }, [tecnoloigas]);
+
+  return (
+    <>
+      <ul>
+        {tecnoloigas.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+
+      <input value={newTch} onChange={e => setNewTch(e.target.value)} />
+
+      <button type="button" onClick={hadleAdd}>
+        Adiciona
+      </button>
+    </>
+  );
 }
 
 export default App;
